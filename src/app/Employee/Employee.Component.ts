e019727ec966f5e8f88fc8f84477ab17e21446c5
NareshReddy11 <nareshreddy.v1994@gmail.com>
2018-11-20 21:33:44 +0530

    first commit

--- src/app/Employee/Employee.Component.ts
+++ src/app/Employee/Employee.Component.ts
@@ -1,16 +1,24 @@
 import {Component} from '@angular/core';
 @Component({
     selector:'Employee',    
-    template:'./Employee/Employee.Component.html',
+    templateUrl :'Employee.Component.html',
+    
 })
 export class EmployeeComponent
 {
     Fname:string='naresh reddy';
     salary:number=30000;
     department:string="training";
+    imgPath:string="C:\Users\Lokesh\Pictures\Camera Roll\public\favPic.jpg"
+  clr:string="red";
+  bgclr:string="skyblue";
+
+    showDetails: boolean= false;
 
     empName() : string{
         return this.Fname +' '+ this.department;
     }
-
+    displayDetails(): void{
+        this.showDetails = !this.showDetails;
+    }
 }
